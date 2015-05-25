0b999b832308381ee86fa0e4363fe6b3fd4c502e
ImtiazEmu <emu@bytelogistics.com>
2015-05-25 12:58:04 +0600

    create dashboard controller, after sign_up redirect to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def after_sign_in_path_for(user)
-  #   user_url(user)
-  # end
+  def after_sign_in_path_for(user)
+    dashboard_index_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   resources :profiles
 
   devise_for :users
