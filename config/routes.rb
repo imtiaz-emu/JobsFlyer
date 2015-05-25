ebdd6a4b4be6c9979a0dcacf55ef4c6cac301a91
ImtiazEmu <emu@bytelogistics.com>
2015-05-25 16:41:54 +0600

    facebook login is done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   resources :profiles
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
