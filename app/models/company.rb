f1f525cf2a567cbeb0ab5fe7c86099ab138a2dbc
ImtiazEmu <emu@bytelogistics.com>
2015-05-31 14:31:49 +0600

    create view for creating new company

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,6 @@ class Company < ActiveRecord::Base
 
   mount_uploader :logo, ProfileUploader
   mount_uploader :bg_image, ProfileUploader
+
+  EMPLOYEE_RANGE = ['1-5', '6-15', '16-30', '30-50', '50+']
 end
