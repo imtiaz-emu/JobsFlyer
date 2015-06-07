8e9cf9c1d5405c70cd80166e682f13f7c8daf898
ImtiazEmu <emu@bytelogistics.com>
2015-06-07 18:45:53 +0600

    subscription index page completed

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -82,7 +82,7 @@ class SubscriptionsController < ApplicationController
     end
 
     def cannot_edit_active_subscriptions
-      if ['pending', 'draft'].include?(@subscription.status)
+      if ['pending', 'draft'].include?(@subscription.status) && @subscription.user == current_user
         redirect_to edit_user_subscription_path(current_user, @subscription)
       else
         redirect_to user_subscriptions_path(current_user), flash: {:error => 'You cannot edit previously subscribed packages.'}
