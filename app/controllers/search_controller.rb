ac035dd4cd7f08d3b93be5477eb612a42a2de3c2
ImtiazEmu <emu@bytelogistics.com>
2015-06-15 15:38:57 +0600

    search with job location done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,9 @@ class SearchController < ApplicationController
   layout 'dashboard'
 
   def index
-    @results = Job.quick_search(params[:q])
+    @results = Job.quick_search(params[:query])
+    if params[:area].present?
+      @results = @results.count == 0 ? Job.where(:job_location => params[:area]) : @results.where(:job_location => params[:area])
+    end
   end
 end
