760368f028c875ba406430746d641aa669165b1b
ImtiazEmu <emu@bytelogistics.com>
2015-06-17 11:28:44 +0600

    deploy via remote cache, jquery_us after bootstrap

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require jquery_ujs
 //= require jquery.min
 //= require jquery.pjax
 //= require transition
--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :use_sudo, false
 # Default value for :scm is :git
 set :scm, :git
 
-# set :deploy_via, :remote_cache, # off while it is first deployment
+set :deploy_via, :remote_cache # off while it is first deployment
 
 # Default value for :format is :pretty
 # set :format, :pretty
