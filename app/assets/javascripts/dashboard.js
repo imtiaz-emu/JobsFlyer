7f6563ae232215d06a9972a0ecef01519e492880
ImtiazEmu <emu@bytelogistics.com>
2015-06-17 11:22:13 +0600

    dashboard widget and secret key file modified

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -13,7 +13,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require jquery
+//= require jquery.min
 //= require jquery.pjax
 //= require transition
 //= require collapse
