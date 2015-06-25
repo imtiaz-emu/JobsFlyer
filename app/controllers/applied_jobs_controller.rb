42a3a72e6593eb28d542e2f45e0288ab403c6de4
imtiaz_Emu <imtiaz.emu.it@gmail.com>
2015-06-25 22:11:21 +0600

    create changes on applied job controller

--- app/controllers/applied_jobs_controller.rb
+++ app/controllers/applied_jobs_controller.rb
@@ -5,10 +5,10 @@ class AppliedJobsController < ApplicationController
       api_url = YAML.load_file('config/api_information.yml')['dropresume']['user'] + '?email=' + current_user.email
       # api_url = YAML.load_file('config/api_information.yml')['dropresume']['user'] + '?email=emu@bytelogistics.com' #+ current_user.email
       user_response = HTTParty.get api_url
-      @blog_posts = blog_response.parsed_response
+      @user_info = blog_response.parsed_response
     rescue Exception => ex
-      Rails.logger.error "Blog load error: #{ex.message}"
-      @blog_posts = []
+      Rails.logger.error "Api load error: #{ex.message}"
+      @user_info = []
     end
   end
 
