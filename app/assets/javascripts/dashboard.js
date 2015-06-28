7d3bb78334aabea11757a75bc990de45974d9771
imtiaz_Emu <imtiaz.emu.it@gmail.com>
2015-06-28 18:42:36 +0600

    dashboard js files reorder

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require jquery_ujs
 //= require jquery.min
-//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require jquery_ujs
 //= require jquery.pjax
 //= require transition
