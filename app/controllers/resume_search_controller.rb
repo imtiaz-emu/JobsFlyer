cdd1f672ee497f5dff68f6b16ef67d3bd2e106e4
imtiaz_Emu <imtiaz.emu.it@gmail.com>
2015-07-14 00:28:39 +0600

    restrict user for resume search with out subscription

--- app/controllers/resume_search_controller.rb
+++ app/controllers/resume_search_controller.rb
@@ -1,5 +1,7 @@
 class ResumeSearchController < ApplicationController
 
+  before_filter :check_has_subscription
+
   layout 'dashboard_widget'
 
   def index
@@ -15,6 +17,21 @@ class ResumeSearchController < ApplicationController
     rescue Exception => ex
       Rails.logger.error "Api load error: #{ex.message}"
       # redirect_to job_path(params[:job_id]), flash: {:error => "API load error!"}
+    end
+  end
+
+  private
+  def check_has_subscription
+    active_subscriptions = current_user.companies.collect{|com| com.subscriptions.active_subscriptions}.flatten
+    if active_subscriptions.count > 0
+      active_subscriptions.each do |subscription|
+        if (subscription.updated_at + subscription.total_month.months) >= Date.today
+          return true
+        end
+      end
+      redirect_to dashboard_path, flash: {:notice => "You're search for resume period expired. Please re-subscribe!"}
+    else
+      redirect_to new_subscription_path, flash: {:error => "You've no active subscriptions. Please subscribe!"}
     end
   end
 end
