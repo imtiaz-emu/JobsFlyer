7212cdb8c65a63adfa57c8d1ace1bb42eaca6820
imtiaz_Emu <imtiaz.emu.it@gmail.com>
2015-07-16 14:29:44 +0600

    delete api_formation yaml file sample exists

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :rvm1_ruby_version, "ruby-2.1.5"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/api_information.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
