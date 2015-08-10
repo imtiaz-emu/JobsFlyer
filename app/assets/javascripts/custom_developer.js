cf94704bdfa8ff93a42664e836135732fc28359d
ImtiazEmu <emu@bytelogistics.com>
2015-08-10 16:03:58 +0600

    Profile edit page About me field data show

--- app/assets/javascripts/custom_developer.js
+++ app/assets/javascripts/custom_developer.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
     $('.comment-body .delete-comment').tooltip();
     $('.comment-body .editing-comment').tooltip();
 })
+
+function focus_text_field_on_edit(evnt){
+
+}
