18786c595e933ff4822e50b0a6e87673ac00b184
ImtiazEmu <emu@bytelogistics.com>
2015-08-10 16:54:39 +0600

    post comments delete/edit condition added

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,15 @@ module CommentsHelper
   def unique_id_generator(record)
     record.class.name.downcase + '__' + record.id.to_s
   end
+
+  def commenter(comment)
+    comment.user == current_user
+  end
+  def poster(post)
+    if post.is_a?(Post)
+      return post.user == current_user
+    else
+      return post.company.users.include?(current_user)
+    end
+  end
 end
