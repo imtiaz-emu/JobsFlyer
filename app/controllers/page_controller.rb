b611809be9331bab8233fdb83d9d269221cb2d67
ImtiazEmu <emu@bytelogistics.com>
2015-08-23 11:17:58 +0600

    error page added 404 & 500

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,4 +2,10 @@ class PageController < ApplicationController
 
   layout 'static_page'
 
+  before_action :load_companies
+
+  def load_companies
+    @top_companies = Company.all.sort{|a,b| b.jobs.count <=> a.jobs.count}.first(24)
+  end
+
 end
